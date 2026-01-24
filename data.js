bfb8f0a0499ffb49895b50a50784261ed4fd8e61
KFruti88 <raykevin71888@gmail.com>
2026-01-23 22:54:18 -0500

    Create data.js

---
+++ data.js
@@ -0,0 +1,31 @@
+// This is your master list for all of Clay County
+const clayCountyData = [
+    { 
+        id: 'angela-florist', 
+        name: "Angela's Village Florist", 
+        town: 'Flora', 
+        category: 'Shopping',
+        phone: '618-662-4411', 
+        logo: 'https://raw.githubusercontent.com/KFruti88/Clay-County-Business-Directory/main/logos/florist.png', 
+        bio: 'Fresh flowers and gifts for the Flora community.' 
+    },
+    { 
+        id: 'wabash-networks', 
+        name: 'Wabash Independent Networks', 
+        town: 'Louisville', 
+        category: 'Services',
+        phone: '618-665-3311', 
+        logo: 'https://raw.githubusercontent.com/KFruti88/Clay-County-Business-Directory/main/logos/wabash.png', 
+        bio: 'Fiber-fast internet and local phone service.' 
+    },
+    { 
+        id: 'sk-venture-signs', 
+        name: 'S&K Venture Signs', 
+        town: 'Flora', 
+        category: 'Services',
+        phone: '', 
+        logo: 'https://raw.githubusercontent.com/KFruti88/Clay-County-Business-Directory/main/logos/sk-signs.png', 
+        bio: 'Digital billboard advertising and repair.' 
+    }
+    // Add every business in Clay County here following this exact format
+];
